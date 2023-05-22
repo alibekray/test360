8c6cd35fa086d32992c3532d2c0fa1f69dfab902
Alibek Raiymbekov <43858443+alibekray@users.noreply.github.com>
2023-05-22 14:12:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const viewer = new PANOLENS.Viewer({
   container: imageContainer,
   autoRotate: true,
   autoRotateSpeed: 0.3,
-  controlBar: false,
+  controlBar: true,
 });
 
 viewer.add(panoramaImage);
